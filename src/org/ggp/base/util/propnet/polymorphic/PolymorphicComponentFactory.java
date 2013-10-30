8ffcca1deba403b3e6b6896b8cfaf95c919fb0d4
SteveDraper <steve.p.draper@gmail.com>
2013-10-30 13:19:36 -0500

    Fully array-ized collectiosn the statemachine needs to enumerate at game time

--- src/org/ggp/base/util/propnet/polymorphic/PolymorphicComponentFactory.java
+++ src/org/ggp/base/util/propnet/polymorphic/PolymorphicComponentFactory.java
@@ -3,10 +3,10 @@ package org.ggp.base.util.propnet.polymorphic;
 import org.ggp.base.util.gdl.grammar.GdlSentence;
 
 public abstract class PolymorphicComponentFactory {
-	public abstract PolymorphicAnd createAnd();
-	public abstract PolymorphicOr createOr();
-	public abstract PolymorphicNot createNot();
-	public abstract PolymorphicConstant createConstant(boolean value);
-	public abstract PolymorphicProposition createProposition(GdlSentence name);
-	public abstract PolymorphicTransition createTransition();
+	public abstract PolymorphicAnd createAnd(int numInputs, int numOutputs);
+	public abstract PolymorphicOr createOr(int numInputs, int numOutputs);
+	public abstract PolymorphicNot createNot(int numOutputs);
+	public abstract PolymorphicConstant createConstant(int numOutputs, boolean value);
+	public abstract PolymorphicProposition createProposition(int numOutputs, GdlSentence name);
+	public abstract PolymorphicTransition createTransition(int numOutputs);
 }
