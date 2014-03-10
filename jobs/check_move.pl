672dc33819026187c92482d9999931f13d470ee7
Andrew Rose <andrewrose+github@gmail.com>
2014-03-10 15:41:25 +0000

    Simple script to check that chosen move is acceptable

---
+++ jobs/check_move.pl
@@ -0,0 +1,19 @@
+use strict;
+
+use JSON;
+
+my $lPlayerIndex = shift;
+my $lAcceptable = shift;
+
+my $lRecord = decode_json <>;
+my $lLastMove = $lRecord->{moves}[-1][$lPlayerIndex];
+$lLastMove =~ s/^\( //;
+$lLastMove =~ s/ \)$//;
+
+if (index($lAcceptable, ";$lLastMove;") == -1)
+{
+  print STDERR "Unacceptable move: $lLastMove\n";
+  exit 1;
+}
+
+exit 0;
