873c385ed646d652e632a9326a0300ab6dcaf406
Andrew Rose <andrewrose+github@gmail.com>
2014-03-25 17:24:28 +0000

    #23 1st step (of many) for heuristics

---
+++ src/org/ggp/base/player/gamer/statemachine/sancho/heuristic/Analyser.java
@@ -0,0 +1,21 @@
+package org.ggp.base.player.gamer.statemachine.sancho.heuristic;
+
+import org.ggp.base.util.propnet.polymorphic.forwardDeadReckon.ForwardDeadReckonInternalMachineState;
+import org.ggp.base.util.statemachine.implementation.propnet.TestForwardDeadReckonPropnetStateMachine;
+
+public abstract class Analyser
+{
+  //	Initialize with a given a state machine to analyse
+  public abstract void init(TestForwardDeadReckonPropnetStateMachine stateMachine);
+
+  //	Accrue a state sample from each state within a rollout
+  public abstract void accrueInterimStateSample(ForwardDeadReckonInternalMachineState state,
+                                                int choosingRoleIndex);
+
+  //	Accrue a rollout sample (terminal state from a rollout)
+  public abstract void accrueTerminalStateSample(ForwardDeadReckonInternalMachineState state,
+                                                 double[] roleScores);
+
+  //	Complete the analysis
+  public abstract void completeAnalysis();
+}
