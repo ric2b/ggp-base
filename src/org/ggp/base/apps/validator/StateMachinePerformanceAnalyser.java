4f47c745041c1e1a1cfd365c952a7cc0e321089c
SteveDraper <steve.p.draper@gmail.com>
2014-04-22 16:16:04 -0500

    Test apps cuurent state (nothing to see here)

--- src/org/ggp/base/apps/validator/StateMachinePerformanceAnalyser.java
+++ src/org/ggp/base/apps/validator/StateMachinePerformanceAnalyser.java
@@ -1,6 +1,9 @@
 package org.ggp.base.apps.validator;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.ggp.base.util.game.GameRepository;
 import org.ggp.base.util.game.LocalGameRepository;
@@ -118,62 +121,111 @@ class PerfTester
 
 public class StateMachinePerformanceAnalyser
 {
-
   public static void main(String[] args)
   {
-    if ( args.length >= 1 )
+    if ( true )
     {
+      Map<String,Integer> gamesList = new HashMap<>();
+      int numSeconds = 30;
+
+      gamesList.put("connect5",0);
+      gamesList.put("reversi",0);
+      gamesList.put("ticTacToe",0);
+      gamesList.put("checkers",0);
+      gamesList.put("pawnWhopping",0);
+      gamesList.put("connect4",0);
+      gamesList.put("connectFourLarger",0);
+      gamesList.put("breakthrough",0);
+      gamesList.put("pentago",0);
+      gamesList.put("quarto",0);
+
       GameRepository theRepository = GameRepository.getDefaultRepository();
       try
       {
-        String gameKey = args[0];
-
-        if ( theRepository.getGameKeys().contains(gameKey))
+        for(String gameKey : gamesList.keySet())
         {
-          //  Instantiate the statemachine to be tested here as per the following commented out
-          //  line in place of the basic prover
-          //TestPropnetStateMachine theMachine = new TestPropnetStateMachine(new LearningComponentFactory());
-          ForwardDeadReckonPropnetStateMachine theMachine = new ForwardDeadReckonPropnetStateMachine();
-
-          System.out.println("Measure game " + gameKey + " state machine performance.");
-
-          List<Gdl> description = theRepository.getGame(gameKey).getRules();
-          theMachine.initialize(description);
-          theMachine.disableGreedyRollouts();
-
-          ForwardDeadReckonInternalMachineState initialState = theMachine.createInternalState(theMachine.getInitialState());
-          Role ourRole = theMachine.getRoles().get(0);
-
-          ProfilerContext.setProfiler(new ProfilerSampleSetSimple());
-          try
+          if ( theRepository.getGameKeys().contains(gameKey))
           {
-            long startTime = System.currentTimeMillis();
-            int numDepthCharges = 0;
+            //  Instantiate the statemachine to be tested here as per the following commented out
+            //  line in place of the basic prover
+            //TestPropnetStateMachine theMachine = new TestPropnetStateMachine(new LearningComponentFactory());
+            ForwardDeadReckonPropnetStateMachine theMachine = new ForwardDeadReckonPropnetStateMachine();
 
-            while(System.currentTimeMillis() < startTime + 10000)
+            System.out.println("Measure game " + gameKey + " state machine performance.");
+
+            List<Gdl> description = theRepository.getGame(gameKey).getRules();
+            theMachine.initialize(description);
+            theMachine.disableGreedyRollouts();
+
+            ForwardDeadReckonInternalMachineState initialState = theMachine.createInternalState(theMachine.getInitialState());
+            Role ourRole = theMachine.getRoles().get(0);
+
+            ProfilerContext.setProfiler(new ProfilerSampleSetSimple());
+            try
             {
-              theMachine.getDepthChargeResult(initialState, null, ourRole, null, null, null);
-              numDepthCharges++;
+              long startTime = System.currentTimeMillis();
+              int numDepthCharges = 0;
+              int[] rolloutStats = null;//new int[2];
+              double averageLen = 0;
+              int minLen = Integer.MAX_VALUE;
+              int maxLen = 0;
+              double averageBranchingFactor = 0;
+              //boolean hasFailed = false;
+
+              while(System.currentTimeMillis() < startTime + 1000*numSeconds)
+              {
+  //              if ( hasFailed )
+  //              {
+  //                System.out.println("Debug now!");
+  //              }
+                theMachine.getDepthChargeResult(initialState, null, ourRole, rolloutStats, null, null);
+                numDepthCharges++;
+
+//                averageLen = (averageLen*(numDepthCharges-1) + rolloutStats[0])/numDepthCharges;
+//                averageBranchingFactor = (averageBranchingFactor*(numDepthCharges-1) + rolloutStats[1])/numDepthCharges;
+//                if ( rolloutStats[0] < minLen )
+//                {
+//                  minLen = rolloutStats[0];
+//                }
+//                if ( rolloutStats[0] > maxLen )
+//                {
+//                  maxLen = rolloutStats[0];
+//                }
+//
+  //              if ( rolloutStats[0] < 2 )
+  //              {
+  //                System.out.println("Failure on iteration " + numDepthCharges);
+  //                hasFailed = true;
+  //              }
+              }
+
+              gamesList.put(gameKey,numDepthCharges/numSeconds);
+//              System.out.println("Performed " + (numDepthCharges/10) + " depth charges per second from initial state");
+//              System.out.println("Game len stats: [" + minLen + ", " + maxLen + "], average: " + averageLen);
+//              System.out.println("Average branching factor: " + averageBranchingFactor);
+//              if (ProfilerContext.getContext() != null)
+//              {
+//                System.out.println("Profile stats: \n" + ProfilerContext.getContext().toString());
+//              }
             }
-
-            System.out.println("Performed " + (numDepthCharges/10) + " depth charges per second from initial state");
-            if (ProfilerContext.getContext() != null)
+            catch (Exception e)
             {
-              System.out.println("Profile stats: \n" + ProfilerContext.getContext().toString());
+              GamerLogger.logStackTrace("StateMachine", e);
             }
           }
-          catch (Exception e)
+          else
           {
-            GamerLogger.logStackTrace("StateMachine", e);
+            System.out.println("Game " + gameKey + " not found");
+            for(String key : theRepository.getGameKeys())
+            {
+              System.out.println(key);
+            }
           }
         }
-        else
+
+        for(Entry<String, Integer> e : gamesList.entrySet())
         {
-          System.out.println("Game " + gameKey + " not found");
-          for(String key : theRepository.getGameKeys())
-          {
-            System.out.println(key);
-          }
+          System.out.println("Game " + e.getKey() + ": " + e.getValue() + " simulations per second");
         }
       }
       finally
