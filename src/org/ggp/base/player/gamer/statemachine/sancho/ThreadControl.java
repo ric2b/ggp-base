aab23bf317e48c5d18dd07c6abbaf602ae88461f
Andrew Rose <andrewrose+github_public@gmail.com>
2014-05-09 19:52:10 +0100

    #50 Don't use all the threads (again)

--- src/org/ggp/base/player/gamer/statemachine/sancho/ThreadControl.java
+++ src/org/ggp/base/player/gamer/statemachine/sancho/ThreadControl.java
@@ -44,12 +44,10 @@ public class ThreadControl
    *
    * !! ARR Put in a method to configure this.
    */
-  /*
   public static final int     CPU_INTENSIVE_THREADS = RUN_SYNCHRONOUSLY ? 1 :
                                                                           HALF_STRENGTH ? ((((NUM_CPUS + 1) / 2) + 1) / 2) :
                                                                                           ((NUM_CPUS + 1) / 2);
-  */
-  public static final int     CPU_INTENSIVE_THREADS = NUM_CPUS; // !! ARR Hack
+  // public static final int     CPU_INTENSIVE_THREADS = NUM_CPUS; // !! ARR Hack
 
   /**
    * The number of rollout threads.
