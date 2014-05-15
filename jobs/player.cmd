c9024364fa342d630ad8719779caf3a595f104ca
SteveDraper <steve.p.draper@gmail.com>
2014-05-15 11:57:57 -0500

    Updated regression scripts for use of external JARs

--- jobs/player.cmd
+++ jobs/player.cmd
@@ -10,4 +10,4 @@ REM %2  - Port
 REM %3  - Player class
 REM %4+ - Optional player-specific configuration
 
-start cmd.exe /C "java -cp bin;bin\external\reflections\reflections-0.9.9-RC1.jar;bin\external\Guava\guava-14.0.1.jar;bin\external\javassist\javassist.jar;bin\external\JUnit\junit-4.11.jar org.ggp.base.apps.player.PlayerRunner %2 %3 %4 %5 %6 %7 %8 %9 >%1 2>&1"
+start cmd.exe /C "java -cp bin;bin\external\reflections\reflections-0.9.9-RC1.jar;bin\external\Guava\guava-14.0.1.jar;bin\external\javassist\javassist.jar;bin\external\JUnit\junit-4.11.jar;bin\external\jna\jna-platform-4.1.0.jar;bin\external\jna\jna-4.1.0.jar;bin\external\disruptor\disruptor-3.2.1.jar org.ggp.base.apps.player.PlayerRunner %2 %3 %4 %5 %6 %7 %8 %9 >%1 2>&1"
--- jobs/sancho.cmd
+++ jobs/sancho.cmd
@@ -2,5 +2,5 @@
 setlocal
 cd ..\bin
 :restart
-call java -cp .;external\reflections\reflections-0.9.9-RC1.jar;external\Guava\guava-14.0.1.jar;external\javassist\javassist.jar;external\JUnit\junit-4.11.jar org.ggp.base.apps.player.PlayerRunner 9147 Sancho
+call java -cp .;external\reflections\reflections-0.9.9-RC1.jar;external\Guava\guava-14.0.1.jar;external\javassist\javassist.jar;external\JUnit\junit-4.11.jar;external\jna\jna-platform-4.1.0.jar;external\jna\jna-4.1.0.jar;external\disruptor\disruptor-3.2.1.jar org.ggp.base.apps.player.PlayerRunner 9147 Sancho
 goto restart
