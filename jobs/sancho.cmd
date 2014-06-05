6352e99fd239704f14f6ef087b319f6f907b73a4
Andrew Rose <andrewrose+github_public@gmail.com>
2014-06-05 23:13:47 +0100

    Fix up working directories and classpaths in Sancho-running scripts

--- jobs/sancho.cmd
+++ jobs/sancho.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-cd ..\bin
+cd ..
 :restart
-call java -cp .;external\reflections\reflections-0.9.9-RC1.jar;external\Guava\guava-14.0.1.jar;external\javassist\javassist.jar;external\JUnit\junit-4.11.jar;external\jna\jna-platform-4.1.0.jar;external\jna\jna-4.1.0.jar;external\disruptor\disruptor-3.2.1.jar;bin\external\Commons\commons-codec-1.9\commons-codec-1.9.jar;bin\external\Commons\commons-compress-1.8.1\commons-compress-1.8.1.jar org.ggp.base.apps.player.PlayerRunner 9147 Sancho
+call java -XX:+UseG1GC -XX:MaxGCPauseMillis=800 -XX:+ExplicitGCInvokesConcurrent -cp bin;bin\external\reflections\reflections-0.9.9-RC1.jar;bin\external\Guava\guava-14.0.1.jar;bin\external\javassist\javassist.jar;bin\external\JUnit\junit-4.11.jar;bin\external\jna\jna-platform-4.1.0.jar;bin\external\jna\jna-4.1.0.jar;bin\external\disruptor\disruptor-3.2.1.jar;bin\external\Commons\commons-codec-1.9\commons-codec-1.9.jar;bin\external\Commons\commons-compress-1.8.1\commons-compress-1.8.1.jar;bin\external\Log4J\log4j-core-2.0-rc1.jar;bin\external\Log4J\log4j-api-2.0-rc1.jar org.ggp.base.apps.player.PlayerRunner 9147 Sancho
 goto restart
