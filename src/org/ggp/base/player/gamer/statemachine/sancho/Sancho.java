5e672233471dc0326fee130a553591ba17a9ddee
Andrew Rose <andrewrose+github_public@gmail.com>
2014-07-27 20:57:12 +0100

    Increment version number

--- src/org/ggp/base/player/gamer/statemachine/sancho/Sancho.java
+++ src/org/ggp/base/player/gamer/statemachine/sancho/Sancho.java
@@ -156,7 +156,7 @@ public class Sancho extends SampleGamer
   @Override
   public String getName()
   {
-    return MachineSpecificConfiguration.getCfgVal(CfgItem.PLAYER_NAME, "Sancho 1.59k.test");
+    return MachineSpecificConfiguration.getCfgVal(CfgItem.PLAYER_NAME, "Sancho 1.59l");
   }
 
   @Override
