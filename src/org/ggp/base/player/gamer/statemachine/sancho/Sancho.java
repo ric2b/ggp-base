6541745290b73a3ec59b231f2622466aae8c360c
SteveDraper <steve.p.draper@gmail.com>
2014-10-09 17:45:09 -0500

    Update version name

--- src/org/ggp/base/player/gamer/statemachine/sancho/Sancho.java
+++ src/org/ggp/base/player/gamer/statemachine/sancho/Sancho.java
@@ -159,7 +159,7 @@ public class Sancho extends SampleGamer
   @Override
   public String getName()
   {
-    return MachineSpecificConfiguration.getCfgVal(CfgItem.PLAYER_NAME, "Sancho 1.60m");
+    return MachineSpecificConfiguration.getCfgVal(CfgItem.PLAYER_NAME, "Sancho 1.60n");
   }
 
   @Override
