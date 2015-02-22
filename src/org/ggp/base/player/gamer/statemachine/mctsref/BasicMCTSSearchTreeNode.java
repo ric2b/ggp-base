52f1d3795aa3f2c74507ec142ff565ae1a4322ae
SteveDraper <steve.p.draper@gmail.com>
2015-02-22 10:15:25 -0600

    Added ref MCTS player + testbed for variation

---
+++ src/org/ggp/base/player/gamer/statemachine/mctsref/BasicMCTSSearchTreeNode.java
@@ -0,0 +1,31 @@
+package org.ggp.base.player.gamer.statemachine.mctsref;
+
+import org.ggp.base.util.propnet.polymorphic.forwardDeadReckon.ForwardDeadReckonInternalMachineState;
+
+public class BasicMCTSSearchTreeNode extends SearchTreeNode
+{
+  public BasicMCTSSearchTreeNode(SearchTree xiTree,
+                                 ForwardDeadReckonInternalMachineState xiState,
+                                 int xiChoosingRole)
+  {
+    super(xiTree, xiState, xiChoosingRole);
+  }
+
+  @Override
+  void updateScore(SearchTreeNode xiChild, double[] playoutResult)
+  {
+    for(int i = 0; i < scoreVector.length; i++)
+    {
+      scoreVector[i] = (scoreVector[i]*numVisits + playoutResult[i])/(numVisits+1);
+    }
+  }
+
+  @Override
+  SearchTreeNode createNode(ForwardDeadReckonInternalMachineState xiState,
+                            int xiChoosingRole)
+  {
+    // TODO Auto-generated method stub
+    return new BasicMCTSSearchTreeNode(tree, xiState, xiChoosingRole);
+  }
+
+}
---
+++ src/org/ggp/base/player/gamer/statemachine/mctsref/SearchTree.java
@@ -0,0 +1,57 @@
+package org.ggp.base.player.gamer.statemachine.mctsref;
+
+import org.ggp.base.util.propnet.polymorphic.forwardDeadReckon.ForwardDeadReckonInternalMachineState;
+import org.ggp.base.util.propnet.polymorphic.forwardDeadReckon.ForwardDeadReckonLegalMoveInfo;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.implementation.propnet.forwardDeadReckon.ForwardDeadReckonPropnetStateMachine;
+
+public abstract class SearchTree
+{
+  private final ForwardDeadReckonPropnetStateMachine stateMachine;
+  private final int numRoles;
+  private SearchTreeNode root;
+  private final double[] playoutScoreBuffer;
+  private final ForwardDeadReckonLegalMoveInfo[] jointMoveBuffer;
+
+  protected abstract SearchTreeNode createRootTreeNode(ForwardDeadReckonInternalMachineState rootState);
+
+  public SearchTree(ForwardDeadReckonPropnetStateMachine xiStateMachine)
+  {
+    stateMachine = xiStateMachine;
+    root = null;
+
+    numRoles = stateMachine.getRoles().length;
+    playoutScoreBuffer = new double[numRoles];
+    jointMoveBuffer = new ForwardDeadReckonLegalMoveInfo[numRoles];
+  }
+
+  public ForwardDeadReckonPropnetStateMachine getStateMachine()
+  {
+    return stateMachine;
+  }
+
+  public int getNumRoles()
+  {
+    return numRoles;
+  }
+
+  public void clear(ForwardDeadReckonInternalMachineState rootState)
+  {
+    root = createRootTreeNode(rootState);
+  }
+
+  public boolean isSolved()
+  {
+    return root != null && root.complete;
+  }
+
+  public void grow()
+  {
+    root.grow(playoutScoreBuffer, jointMoveBuffer);
+  }
+
+  public Move getBestMove()
+  {
+    return root.getBestMove();
+  }
+}
