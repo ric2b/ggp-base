2bfb873564a724ba526c959b203f5ca8ce395a18
Andrew Rose <andrewrose+github_public@gmail.com>
2015-05-12 22:32:37 +0100

    Fix propnet sizes

--- src/org/ggp/base/test/PropNetSizeTest.java
+++ src/org/ggp/base/test/PropNetSizeTest.java
@@ -25,17 +25,17 @@ public class PropNetSizeTest extends Assert
   {
     LinkedList<Object[]> lTests = new LinkedList<>();
 
-    lTests.add(new Object[] {"base", "ticTacToe",      244,   168,   168,    57});
-    lTests.add(new Object[] {"base", "connectFour",    765,   619,   619,   299});
-    lTests.add(new Object[] {"base", "breakthrough",  1844,  1203,  1203,   142});
+    lTests.add(new Object[] {"base", "ticTacToe",      244,   168,   168,    59});
+    lTests.add(new Object[] {"base", "connectFour",    765,   619,   619,   301});
+    lTests.add(new Object[] {"base", "breakthrough",  1844,  1203,  1203,   144});
     lTests.add(new Object[] {"base", "sudokuGrade1",  5483,  4446,  4399,  1722});
     lTests.add(new Object[] {"base", "checkers",      8079,  4462,  4462,  1571});
-    lTests.add(new Object[] {"base", "reversi",      17795,  3997,  3997, 15195});
+    lTests.add(new Object[] {"base", "reversi",      17795,  3997,  3997, 15197});
     lTests.add(new Object[] {"base", "speedChess",   35416, 15710, 15710,  9512});
     lTests.add(new Object[] {"base", "skirmishNew",  35541, 15705, 15705,  9567});
-    lTests.add(new Object[] {"base", "skirmish",     54143, 39649, 39649,  8897});
-    lTests.add(new Object[] {"base", "hex",          81507, 61164, 61164,  3250});
-    lTests.add(new Object[] {"base", "hexPie",       83023, 82342, 82335,  3300});
+    lTests.add(new Object[] {"base", "skirmish",     54143, 39649, 39649,  8899});
+    lTests.add(new Object[] {"base", "hex",          81507, 61164, 61164,  3902});
+    lTests.add(new Object[] {"base", "hexPie",       83023, 82342, 82335,  3955});
 
     return lTests;
   }
