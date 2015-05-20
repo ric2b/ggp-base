7bb02b13c4c787e19261dcfdd15d622b204604ce
SteveDraper <steve.p.draper@gmail.com>
2015-05-19 20:42:24 -0500

    #254 Fixed state equality identification failure that caused some puzzles to fail

--- src/org/ggp/base/util/propnet/polymorphic/forwardDeadReckon/ForwardDeadReckonInternalMachineState.java
+++ src/org/ggp/base/util/propnet/polymorphic/forwardDeadReckon/ForwardDeadReckonInternalMachineState.java
@@ -407,11 +407,76 @@ public class ForwardDeadReckonInternalMachineState implements ForwardDeadReckonC
   {
     if ( !hashCached )
     {
-      cachedHashCode = contents.hashCode();
+      cachedHashCode = basePropsHash();
       hashCached = true;
     }
 
     return cachedHashCode;
+  }
+
+  //  Only want to hash the bits that correspond to the base props - use OpenBitSet
+  //  code slightly modified to handle this
+  private int basePropsHash()
+  {
+    int firstIndex = (firstBasePropIndex >> 6);
+    int modulo = (firstBasePropIndex & 0x3F);
+    long h;
+    long[] bits = contents.getBits();
+    // Start with a zero hash and use a mix that results in zero if the input is zero.
+    // This effectively truncates trailing zeros without an explicit check.
+    if ( modulo != 0 )
+    {
+      h = 1 << (modulo-1);
+      h |= (h-1);
+      h = ~h;
+      h &= bits[firstIndex++];
+      h = (h << 1) | (h >>> 63); // rotate left
+    }
+    else
+    {
+      h = 0;
+    }
+    for (int i = firstIndex; i<bits.length;i++) {
+      h ^= bits[i];
+      h = (h << 1) | (h >>> 63); // rotate left
+    }
+    // fold leftmost bits into right and add a constant to prevent
+    // empty sets from returning 0, which is too common.
+    return (int)((h>>32) ^ h) + 0x98761234;
+  }
+
+  //  Check for equality in the base props
+  private boolean basePropsEquals(ForwardDeadReckonInternalMachineState other)
+  {
+    int firstIndex = (firstBasePropIndex >> 6);
+    int modulo = (firstBasePropIndex & 0x3F);
+    long h;
+    long[] bits = contents.getBits();
+    long[] otherBits = other.contents.getBits();
+    // Start with a zero hash and use a mix that results in zero if the input is zero.
+    // This effectively truncates trailing zeros without an explicit check.
+    if ( modulo != 0 )
+    {
+      h = 1 << (modulo-1);
+      h |= (h-1);
+      h = ~h;
+      if ( (h & bits[firstIndex]) != (h & otherBits[firstIndex]) )
+      {
+        return false;
+      }
+
+      firstIndex++;
+    }
+
+    for (int i = firstIndex; i<bits.length;i++)
+    {
+      if ( bits[i] != otherBits[i] )
+      {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   @Override
@@ -426,7 +491,7 @@ public class ForwardDeadReckonInternalMachineState implements ForwardDeadReckonC
     {
       ForwardDeadReckonInternalMachineState state = (ForwardDeadReckonInternalMachineState)o;
 
-      return state.contents.equals(contents);
+      return basePropsEquals((ForwardDeadReckonInternalMachineState)o);
     }
 
     return false;
@@ -456,7 +521,7 @@ public class ForwardDeadReckonInternalMachineState implements ForwardDeadReckonC
 
     if (other !=null && hashCode() == other.hashCode())
     {
-      return other.contents.equals(contents);
+      return basePropsEquals(other);
     }
 
     return false;
