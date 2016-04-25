e88ceb28bc36d7ffd8ba1ff3c2f786e2919686e3
Andrew Rose <andrewrose+github@gmail.com>
2016-04-25 22:19:46 +0100

    #383 Tweaks to database parsing

--- src_py/keras/readdb.py
+++ src_py/keras/readdb.py
@@ -3,25 +3,38 @@ import struct
 struct_fmt = '>??idi3Q'
 struct_len = struct.calcsize(struct_fmt)
 struct_unpack = struct.Struct(struct_fmt).unpack_from
+print struct_len, "bytes per record."
 
-print struct_len
+lNumTerminal = 0
+lNumComplete = 0
+lNumEstimate = 0
+lNumTerminalWins   = 0
+lNumTerminalLosses = 0
+lNumCompleteWins   = 0
+lNumCompleteLosses = 0
 
-num_terminal = 0
-num_complete = 0
-num_estimate = 0
-
-results = []
 with open("states.db", "rb") as f:
     while True:
         data = f.read(struct_len)
         if not data: break
-        s = struct_unpack(data)
-        if s[0]:
-          num_terminal += 1
-        elif s[1]:
-          num_complete += 1
+        (lTerminal, lComplete, lVisits, lScore, lStateSize, lState0, lState1, lState2) = struct_unpack(data)
+        assert(lStateSize == 3)
+        #print lTerminal, lComplete, lVisits, lScore, bin(lState0), bin(lState1), bin(lState2)
+        if lTerminal:
+          lNumTerminal += 1
+          if lScore > 99:
+            lNumTerminalWins += 1
+          else:
+            lNumTerminalLosses += 1
+        elif lComplete:
+          lNumComplete += 1
+          if lScore > 99:
+            lNumCompleteWins += 1
+          else:
+            lNumCompleteLosses += 1
         else:
-          num_estimate += 1
-        results.append(s)
+          lNumEstimate += 1
 
-print "Terminal: ", num_terminal, ", Complete: ", num_complete, ", Estimate: ", num_estimate
+print "Terminal:", lNumTerminal, "of which", lNumTerminalWins, "wins and", lNumTerminalLosses, "losses."
+print "Complete:", lNumComplete, "of which", lNumCompleteWins, "wins and", lNumCompleteLosses, "losses."
+print "Estimate:", lNumEstimate
